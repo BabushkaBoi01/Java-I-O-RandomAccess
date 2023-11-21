ade999d67542bd0f0eafe21f299473f36461a9b2
RabiuAmir <95980708+BabushkaBoi01@users.noreply.github.com>
2023-11-21 17:41:49 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    Student cmpe411 = new Student();
+
+    cmpe411.modify_Cgpa("1234","2.54");
+
+    }
+}
